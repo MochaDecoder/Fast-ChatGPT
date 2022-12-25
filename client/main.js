706e195f0549fe86599af03b6be14087592a8de9
MochaDeveloper <aero.passakorn.ke@gmail.com>
2022-12-25 16:39:39 +0700

    remove unnecessary file

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -19,5 +18,3 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
