5c02cde848119cb8b491644dabbdf436af2f3db3
MochaDeveloper <aero.passakorn.ke@gmail.com>
2022-12-25 18:17:31 +0700

    update client: fetch data and parse result

--- client/script.js
+++ client/script.js
@@ -21,7 +21,7 @@ const typeText = (element, text) => {
 
   let interval = setInterval(() => {
     if(index < text.trim().length) {
-      element.innerHtml += text.charAt(index);
+      element.innerHTML += text.charAt(index);
       index++;
     } else {
       clearInterval(interval);
@@ -70,6 +70,25 @@ const handleSubmit = async (e) => {
 
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
+
+  const response = await fetch('http://localhost:5001', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ prompt: data.get('prompt')})
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = 'Something went wrong';
+  }
+
 }
 
 form.addEventListener('submit', handleSubmit);
