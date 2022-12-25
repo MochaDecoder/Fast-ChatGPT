ac2eaa0f1b64be7fa907f5c7e00659a4cdf9085c
MochaDeveloper <aero.passakorn.ke@gmail.com>
2022-12-25 22:28:52 +0700

    update api endpoints

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5001', {
+  const response = await fetch('https://fast-chatgpt-api.onrender.com', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ prompt: data.get('prompt')})
